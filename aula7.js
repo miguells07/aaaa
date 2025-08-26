3d2bfb54706ca543a1b7d55198203ca98ec05633
miguells07 <miguelchagas955@gmail.com>
2025-08-26 16:17:46 -0300

    fim de atividade

--- aula7.js
+++ aula7.js
@@ -7,3 +7,33 @@ console.log("Último time:", timesDeFutebol.at(-1));
 let minhaListaDeTarefas = ["Comprar pão", "Estudar JavaScript", "Fazer exercícios"];
 console.log("Lista de tarefas:", minhaListaDeTarefas);
 
+// Exercício 3
+let animais = ["cachorro", "gato", "papagaio", "coelho", "tartaruga"];
+for (let i = 0; i < animais.length; i++) {
+  console.log(`O animal na posição ${i} é ${animais[i]}`);
+}
+
+// Exercício 4
+let numeros = [7, 12, 5, 20, 15, 8, 3, 22, 11, 4];
+for (let i = 0; i < numeros.length; i++) {
+  if (numeros[i] % 2 === 0) {
+    console.log(numeros[i]);
+  }
+}
+// Exercício 5
+let frutas = ["maçã", "banana", "uva", "morango", "abacaxi"];
+let i = 0;
+while (i < frutas.length) {
+  console.log(frutas[i]);
+  i++;
+}
+console.log("Fim do Desafio!");
+
+// Exercício 6
+for (let num = 2; num <= 9; num++) {
+  let linha = "";
+  for (let mult = 1; mult <= 10; mult++) {
+    linha += `${num} x ${mult} = ${num * mult}  `;
+  }
+  console.log(linha);
+}
