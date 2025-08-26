ae71f9f1796a9be06253b2b5f54b3b45bfe8f50d
miguells07 <miguelchagas955@gmail.com>
2025-08-26 16:05:05 -0300

    atividadee

---
+++ aula7.js
@@ -0,0 +1,9 @@
+// Exercício 1
+let timesDeFutebol = ["Flamengo", "Palmeiras", "Grêmio", "São Paulo"];
+console.log("Primeiro time:", timesDeFutebol[0]);
+console.log("Último time:", timesDeFutebol.at(-1));
+
+// Exercício 2
+let minhaListaDeTarefas = ["Comprar pão", "Estudar JavaScript", "Fazer exercícios"];
+console.log("Lista de tarefas:", minhaListaDeTarefas);
+
